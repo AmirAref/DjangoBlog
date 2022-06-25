723fd188f25b25edac0c4fdae32e1341e36f4743
Amir Aref <amiraref808@gmail.com>
2022-06-26 00:55:11 +0430

    add the categories in the template
    make nevigation bar for categores in base.html
    display the categories of each post in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Category, Post
 
 
 # Create your views here.
 def home(request):
     contex = {
         'posts' : Post.objects.filter(status='p'),
+        'categories' : Category.objects.filter(status=True),
     }
     return render(request, 'blog/home.html', contex)
 
