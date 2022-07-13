9292e9640c0d47f520521ffa4691974eb2881641
Amir Aref <amiraref808@gmail.com>
2022-07-13 13:13:53 +0430

    setup the login page for the account app
    set the LOGIN_REDIRECT_URL and LOGIN_URL in the config/settings.py
    add home view in the account app views.py
    set login url with the LoginView in the acoount/urls.py
    set home url in the account/urls.py
    add login.html and home.html in the templates/regitstion

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .views import home
+
+app_name = 'account'
+
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(), name='login'),  
+    path('', home, name='home'),    
 ]
--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# login urls
+LOGIN_REDIRECT_URL = 'account:home'
+LOGIN_URL = 'account:login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
