067a41ceb9eb4be379ca65c84ff829728f066f0d
Amir Aref <amiraref808@gmail.com>
2022-07-14 16:55:15 +0430

    add new properties to User model
    add is_author boolean field property to User model
    add special_subscription property to User model
    add is_special_user method to check is time of special subscription in User model
    add the is_author and soecial_subscription to the UserAdmin fieldsets in admin.py
    add the is_author and is_special_user to the UserAdmin display_list in admin panel

--- account/admin.py
+++ account/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+# write your models admins here.
+UserAdmin.fieldsets[2][1]['fields'] =  (
+    'is_active', 
+    'is_staff', 
+    'is_superuser', 
+    'is_author',
+    'special_subscription',
+    'groups', 
+    'user_permissions'
+    )
+
+UserAdmin.list_display += ('is_author', 'is_special_user', )
+
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    is_author = models.BooleanField(default=False, verbose_name="وضعیت نویسندگی")
+    special_subscription = models.DateTimeField(default=timezone.now, verbose_name='اشتراک ویژه')
+
+    # boolean field of special subscription
+    def is_special_user(self):
+        if self.special_subscription > timezone.now():
+            return True
+        return False
+    is_special_user.boolean = True
+    is_special_user.short_description = "وضعیت کاربر ویژه"
