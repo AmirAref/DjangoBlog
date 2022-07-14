cdec75e9be92b64d1acd8f11f75737746e413f6b
Amir Aref <amiraref808@gmail.com>
2022-07-15 03:11:29 +0430

    add the PostDelete view
    create new view PostDelete in account views.py
    set the post-delete url to PostDelete view in urls.py
    add the trash button for superusers in the home.html template
    create post_confirm_delete.html page in the template to confirm delete

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import PostList, PostCreate, PostUpdate
+from .views import PostList, PostCreate, PostUpdate, PostDelete
 
 app_name = 'account'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', PostList.as_view(), name='home'),   
     path('create', PostCreate.as_view(), name='post-create'),
     path('update/<int:pk>', PostUpdate.as_view(), name='post-update'),
+    path('delete/<int:pk>', PostDelete.as_view(), name='post-delete'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .mixins import FieldsMixin, FormValidMixin, AuthorAccessMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Post
 
 # Create your views here.
@@ -24,3 +25,8 @@ class PostCreate(LoginRequiredMixin, FieldsMixin, FormValidMixin, CreateView):
 class PostUpdate(AuthorAccessMixin, FieldsMixin, FormValidMixin, UpdateView):
     model = Post
     template_name = 'registration/post-create-update.html'
+
+class PostDelete(AuthorAccessMixin, FieldsMixin, FormValidMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('account:home')
+    template_name = 'registration/post_confirm_delete.html'
