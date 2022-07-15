614fac382e36863821b469d83ba7239db4b41a08
Amir Aref <amiraref808@gmail.com>
2022-07-15 10:42:23 +0430

    add the Logout view
    set the logout url for Logout class view in urls.py
    set the logout redirect url in the project settings.py
    add the logout icon in the base.html template

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import PostList, PostCreate, PostUpdate, PostDelete
 
@@ -6,6 +6,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),  
+    path('logout/', LogoutView.as_view(), name='logout'),  
     path('', PostList.as_view(), name='home'),   
     path('create', PostCreate.as_view(), name='post-create'),
     path('update/<int:pk>', PostUpdate.as_view(), name='post-update'),
--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # login urls
 LOGIN_REDIRECT_URL = 'account:home'
 LOGIN_URL = 'account:login'
+LOGOUT_REDIRECT_URL = 'account:login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
