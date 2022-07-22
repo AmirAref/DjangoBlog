8020c6480845a4e31b8afe6e5f5ab2f6fc13e075
Amir Aref <amiraref808@gmail.com>
2022-07-22 18:09:33 +0430

    create hot posts of the month
    rename the partials/popular_posts.html to side.html
    create new custom hot posts tag
    display the hot and popular posts in the list.html and post detail page
    fix some styles for the popular posts

--- blog/templatetags/base_tags.py
+++ blog/templatetags/base_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count, Q
+from django.contrib.contenttypes.models import ContentType
 from datetime import datetime, timedelta
 
 from ..models import Category, Post
@@ -24,14 +25,29 @@ def field_errors(errors):
     return {'errors' : errors}
 
 
-@register.inclusion_tag('blog/partials/popular_posts.html')
+@register.inclusion_tag('blog/partials/side.html')
 def popular_posts():
     # return the posts of the last month sort by views count
     last_month = datetime.today() - timedelta(days=30)
     return {
-        'popular_posts' : Post.objects.published().annotate(
+        'title' : "پُست های پر بازدید ماه",
+        'posts' : Post.objects.published().annotate(
             count=Count(
-                'hits', # add new 'count' field 
+                'hits', # add new 'count' field -> views count
                 filter=Q(posthit__created__gt=last_month), # filter only posts that their created view time greater than last month
             )).order_by('-count', '-publish')[:5] # sort the posts by their views and then by the publish time
+        }
+
+@register.inclusion_tag('blog/partials/side.html')
+def hot_posts():
+    # return the posts of the last month sort by comments count
+    last_month = datetime.today() - timedelta(days=30)
+    content_type_id = ContentType.objects.get(app_label='blog', model='post').id
+    return {
+        'title' : "پُست های داغ ماه",
+        'posts' : Post.objects.published().annotate(
+            count=Count(
+                'comments', # add new 'count' field -> comments count
+                filter=Q(comments__posted__gt=last_month) and Q(comments__content_type_id=content_type_id), # filter only posts that their created view time greater than last month
+            )).order_by('-count', '-publish')[:5] # sort 
         }
