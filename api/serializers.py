07fa94173b92e1d57ab36c7defb510d440c35eff
Amir Aref <amiraref808@gmail.com>
2022-08-22 09:01:27 +0430

    chore: display the author and category name instead the id in api

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,20 @@ from rest_framework import serializers
 from blog.models import Post
 
 class PostSerilizer(serializers.ModelSerializer):
+    author = serializers.CharField(read_only=True, source='author.username')
+    category = serializers.StringRelatedField(read_only=True, many=True)
+    
     class Meta:
         model = Post
-        exclude = (
-            'created',
-            'hits',
-            )
+        fields = (
+            'title',
+            'slug',
+            'description',
+            'publish',
+            'updated',
+            'thumbnail',
+            'is_special',
+            'status',
+            'author',
+            'category',
+        )
