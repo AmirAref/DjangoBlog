3ead2cb042aebc2662e31cc5aed59c2fd867e894
Amir Aref <amiraref808@gmail.com>
2023-08-03 22:15:16 +0330

    fix: static files serving fix

--- config/settings.py
+++ config/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
